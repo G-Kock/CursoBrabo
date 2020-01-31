15b8bd24b27052c62a294184445cfba44ca6d997
Gabriel Kock <gabrikock@hotmail.com>
2020-01-31 16:16:44 -0300

    .

--- GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
+++ GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
@@ -10,7 +10,7 @@ namespace GitCapirotescoTeste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("O curso é MUITO brabo !!!!!! fasdfas=) ");
+            Console.WriteLine("O curso é MUITO brabo !!!!!!");
         }
     }
 }
